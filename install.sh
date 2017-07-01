98bb8279aad156aff4fff3e396da90c0adf34ed6
svlentink <sv@lent.ink>
2017-07-01 11:52:41 +0000

    cron instead of rc.local

--- install.sh
+++ install.sh
@@ -34,58 +34,57 @@ echo Creating script $SCRIPTNAME
 
 cat <<EOF> $SCRIPTPATH
 #!/bin/bash
-echo Starting the sound processing engine
-[[ -z "\$(ps aux|grep -i fluid|grep -v grep)" ]] \
-&& nohup fluidsynth \
-  --audio-driver=alsa \
-  -o audio.alsa.device=hw:0 \
-  --gain=\$(cat $VOLUMELOC) \
-  --server \
-  --no-shell \
-  /usr/share/sounds/sf2/FluidR3_GM.sf2 \
-  > /dev/null 2>&1 &
-sleep 20
-echo The midi processing engine should be up by now
-
-#echo Listing all audio devices:
-#cat /proc/asound/cards
-#aplay -l
-#echo Listing midi:
-#amidi -l
-echo Listing the midi output devices:
-aplaymidi -l
-echo Listing input connections:
-aconnect -i
-echo Listing output connections:
-aconnect -o
-
 function communicateErr {
   echo \$@
   espeak --stdout \$@ | aplay || true
-  sleep 60
-  ./\$0
 }
 
 INPUTkeyword="usb"
-OUTPUTkeyword="FLUID" #"Midi Through"
 MidiIN=\$(aconnect -i | grep -i "\$INPUTkeyword" | head -1 | awk '{print \$2;}')"0" # something like '20:'"0" = 20:0
-MidiOUT=\$(aplaymidi -l | grep -i "\$OUTPUTkeyword" | head -1 | awk '{print \$1;}') # something like '14:0'
-
 [[ -z "\$MidiIN" ]] \
-  && communicateErr "Input missing, trying again in a minute" \
-  && exit 1
-[[ -z "\$MidiOUT" ]] \
-  && communicateErr "Output missing, trying again in a minute" \
+  && communicateErr "Input missing, trying again in a minute. Please connect keyboard." \
   && exit 1
 
-echo We will use \$MidiIN as input and \$MidiOUT as output
-aconnect \$MidiIN \$MidiOUT
+if [[ -z "\$(ps aux|grep -i fluid|grep -v grep)" ]]; then
+  communicateErr "the audio processor was down, starting it"
+  nohup fluidsynth \
+    --audio-driver=alsa \
+    -o audio.alsa.device=hw:0 \
+    --gain=\$(cat $VOLUMELOC) \
+    --server \
+    --no-shell \
+    /usr/share/sounds/sf2/FluidR3_GM.sf2 \
+    > /dev/null 2>&1 || true &
+  exit 1
+fi
+
+OUTPUTkeyword="FLUID" #"Midi Through"
+MidiOUT=\$(aplaymidi -l | grep -i "\$OUTPUTkeyword" | head -1 | awk '{print \$1;}') # something like '14:0'
+if [[ -z "\$MidiOUT" ]]; then
+  communicateErr "Output missing, trying again in a minute"
+
+  #echo Listing all audio devices:
+  #cat /proc/asound/cards
+  #aplay -l
+  #echo Listing midi:
+  #amidi -l
+  echo Listing the midi output devices:
+  aplaymidi -l
+  echo Listing input connections:
+  aconnect -i
+  echo Listing output connections:
+  aconnect -o
+  
+  exit 1
+else
+  echo We will use \$MidiIN as input and \$MidiOUT as output
+  aconnect \$MidiIN \$MidiOUT
+fi
 
 exit 0
 EOF
 chmod +x $SCRIPTPATH
-[[ -z "$(grep -i enableMidi /etc/rc.local)" ]] \
- && sed -i 's/exit/sleep\ 10\ \&\&\ \/usr\/bin\/enableMidiMusic\nexit/g' /etc/rc.local #running it at startup
+echo "* * * * * root $SCRIPTPATH" > /etc/cron.d/keyboardcron
 
 echo You can change the volume at $VOLUMELOC which requires a reboot
 echo -n "1.5" > $VOLUMELOC
